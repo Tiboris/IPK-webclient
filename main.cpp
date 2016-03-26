4c49855e05340199655222fbcbe08d4a0ccd24be
Tiboris <xdudla00@vutbr.cz>
2016-03-27 00:16:35 +0100

    working with rexes but it works different than on regex101.com

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#include <regex>
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,20 +15,30 @@ using namespace std;
 
 const string FILE_NAME="index.html";
 const int REQ_ARGC=2;
-const int MAX_ATTEMPTS=5;
-//const string "^http:\/\/(www\.)?(.+?(\:\d+)?\/)(.*)$"
+const int MAX_ATTEMPTS=5;	
+const regex rex("^http://(www.)?(.+?)(:d+)?(/.*)$"); // tento regex treba TUTO opravit zerie aj :
+//     tento ide ^http:\/\/(www\.)?([^\:]+?)(\:\d+)?(\/.*)$
 
 
 
 void err_print(string msg, int err_code);
-bool valid_arg(int argc, char const *argv[]);
+bool valid_arg(int count, char const *url[]);
 
 int main(int argc, char const *argv[])
-{
+{	
 	if ( ! valid_arg(argc,argv) )
 	{
-		err_print("",9);
+		err_print("Wrong parameters err ",9);
 		return 1;
+	}
+	smatch matches;
+	string url = argv[1];
+	regex_match (url, matches, rex);
+	cout << "string object with " << matches.size() << " matches\n";
+	cout << "the matches were: " << endl;
+	for (unsigned i=0; i<matches.size(); ++i) 
+	{
+		std::cout << "[" << matches[i] << "] "<< endl;
 	}
 	return 0;
 }
@@ -36,10 +48,7 @@ void err_print(string msg, int err_code)
 	cerr << msg << err_code << endl;	
 }
 
-bool valid_arg(int argc, char const *argv[])
+bool valid_arg(int count, char const *url[])
 {
-	if ((argc == REQ_ARGC) && (argv[1])==0)
-		return true;
-	else
-		return false;
+	return ((count == REQ_ARGC) && (regex_match (url[1],rex))) ? true : false;
 }
