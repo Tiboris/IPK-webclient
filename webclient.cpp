5e78f22da5428a98747607eafff5fdfdbc075668
Tiboris <xdudla00@vutbr.cz>
2016-04-02 18:57:11 +0200

    redirects working TODO: chunks

--- webclient.cpp
+++ webclient.cpp
@@ -1,50 +1,50 @@
 #include <cstdlib>
-
 #include <iostream>
 #include <string>
-
 #include <regex>
-
 #include <netinet/in.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
+/*
+*/
 using namespace std;
-
+/*
+*/
 const int U_PORT = 0;
 const int U_PATH = 1;
 const int F_NAME = 2;
-
 const int REQ_ARGC = 2;
 const int MAX_ATTEMPTS = 5;
-const size_t BUFF_SIZE = 1024;
-
+const size_t BUFF_SIZE = 2048;
 const string DEF_PORT = "80";
 const string DEF_PATH = "/";
 const string DEF_NAME = "index.html";
-string filename;
-
+string file_name;
+int attempts = 0; 
+/*
+*/
 const regex url_rex("^http://(www\\.)?([\\w\\.]+)(\\:(\\d+))?(\\/.*)?$");
 const regex head_rex("HTTP\\/1.(\\d) (\\d+) (.*)");
-
+const regex chunk_rex("Transfer-Encoding: chunked");
+const regex locattion_rex("Location: (.*)");
+/*
+*/
 void err_print(const char *msg);
 string get_default(string str, int type);
-smatch get_rex_matches(string url, regex rex, bool search);
+smatch get_rex_matches(string url, regex rex);
 bool send_req(int sckt, string url, string file_path, int cnt, string ver="1");
+string get_cont(int sckt, string url, string file_path, string ver);
 bool connect_to(smatch url_parts, int attempts);
-bool test_function();
-
-//TEST
-string get_cont(string type, int sckt, string url, string file_path, string ver);
+bool save_response(string response, bool chunked);
+string url_encode(const string input);
 /*
 *
 *
 */
 int main(int argc, char const *argv[])
 {   
-    string filename;
     if ( ! ( argc == REQ_ARGC ) ) {
         err_print("Wrong parameters, usage: ./webclient url");
         return EXIT_FAILURE;
@@ -53,14 +53,11 @@ int main(int argc, char const *argv[])
         err_print("Bad url");
         return EXIT_FAILURE;
     }
-    string url = argv[1]; //
-    int attempts = 0; 
-    if ( connect_to(get_rex_matches(url,url_rex,false), ++attempts) ) {
+    string url = argv[1]; 
+    if ( connect_to(get_rex_matches(url,url_rex), ++attempts) ) {
         return EXIT_FAILURE;
     }
     return EXIT_SUCCESS;
-    // 1.1 chunk
-    // 1.0 no chunk
 }
 /*
 *
@@ -68,18 +65,19 @@ int main(int argc, char const *argv[])
 */
 bool connect_to(smatch url_parts, int attempts)
 {   // [0]full_url [1]www [2]domain [4]port [5]path
+    const string path = get_default(static_cast<string>(url_parts[5]), U_PATH);
+    size_t pos = path.find_last_of("/");
+    if (attempts == 1)
+    {
+        file_name = get_default(path.substr(pos+1), F_NAME );
+    }
     if (attempts > MAX_ATTEMPTS) {
         err_print("Too many redirects");
         return EXIT_FAILURE;
     }
-    const string sub  = static_cast<string>(url_parts[1]);
-    const string url  = static_cast<string>(url_parts[2]);
     const string port = get_default(static_cast<string>(url_parts[4]),U_PORT );
-    const string full_path = static_cast<string>(url_parts[5]);
-    const size_t found = full_path.find_last_of("/");
-    const string path = get_default(full_path.substr(0,found+1),U_PATH );
-    filename = get_default(full_path.substr(found+1), F_NAME );
-
+    const string url  = static_cast<string>(url_parts[1]) + static_cast<string>(url_parts[2]);
+    
     bool send_err;
     int code, sckt;
     struct addrinfo set;
@@ -91,7 +89,7 @@ bool connect_to(smatch url_parts, int attempts)
     set.ai_protocol = 0;           /* Any protocol */
     set.ai_flags = 0;
 
-    if ((code = getaddrinfo( (sub + url).c_str(), port.c_str(), &set, &results) ) != 0) { 
+    if ((code = getaddrinfo( url.c_str(), port.c_str(), &set, &results) ) != 0) { 
         err_print( gai_strerror(code) );
         return EXIT_FAILURE;
     }
@@ -112,25 +110,83 @@ bool connect_to(smatch url_parts, int attempts)
         return EXIT_FAILURE;
     }
     int s_att = 1;
-    send_err = send_req(sckt, (sub+url), full_path, s_att);
-    close(sckt); 
-    if (send_err){
+    send_err = send_req(sckt, url, path, s_att); // send is closing socket
+    return (send_err) ? EXIT_FAILURE : EXIT_SUCCESS;
+}
+/*
+*
+*
+*/
+bool send_req(int sckt, string url, string file_path, int cnt, string ver)
+{
+    string resp_msg = get_cont(sckt, url, url_encode(file_path), ver);
+    close(sckt);
+    if (resp_msg == "ErrInMyGet")
+    {
         return EXIT_FAILURE;
     }
-    else{
+    smatch head;
+    regex_search(resp_msg, head, head_rex);
+    string res_ver = static_cast<string>(head[1]);
+    string s_code = static_cast<string>(head[2]);
+    int code = atoi(s_code.c_str());
+    string err_msg = static_cast<string>(head[3]);
+    
+    cout << "----------" << endl;
+    cout << "Pokus "<< attempts << " file " << file_path << endl << "Head_ver " << ver << endl ;
+    cout << "----------" << endl;
+    if (code == 302 || code == 301) { 
+        regex_search(resp_msg, head, locattion_rex);
+        string url = static_cast<string>(head[1]);
+        if (connect_to(get_rex_matches(url,url_rex), ++attempts) ) { 
+            return EXIT_FAILURE;
+        }
+        else {
+            return EXIT_SUCCESS;            
+        }
+    }
+    if (code > 399 && code < 600) {
+        err_print((s_code + " " + err_msg).c_str());
+        return EXIT_FAILURE;
+    }
+    if (code == 200) {
+        bool chunked = regex_search(resp_msg, head, chunk_rex);
+        if ((cnt == 1) && (res_ver=="1")) {
+            if (save_response(resp_msg, chunked)){
+                return EXIT_FAILURE;
+            }
+        }
+        if ((cnt == 1) && (res_ver == "0")) {
+            if( send_req(sckt, url, file_path, ++cnt, "0") ){
+                return EXIT_FAILURE;
+            }
+        }
+        if ( (cnt == 2) && (res_ver == "0")) {
+            if (save_response(resp_msg, chunked)) {
+                return EXIT_FAILURE;
+            }
+        }
+        if ((cnt == 2) && (res_ver != "0")) {
+            err_print("Unexpected error");
+            return EXIT_FAILURE;
+        }
         return EXIT_SUCCESS;
+    }
+    else{
+        err_print((s_code + " " + err_msg).c_str());
+        return EXIT_FAILURE;
     }
 }
 /*
 *
 *
 */
-string get_cont(string type, int sckt, string url, string file_path, string ver){
+string get_cont( int sckt, string url, string file_path, string ver){
     int res;
     char response[BUFF_SIZE];
     bzero(response, BUFF_SIZE);
     string resp_msg = "";
-    string msg =    type + " " + file_path + " HTTP/1." + ver + "\r\n" + 
+    string msg =    "GET " + file_path + " HTTP/1." + ver + "\r\n" + 
                     "Host: " + url + "\r\n" + "Connection: close\r\n\r\n";
     if ( send(sckt, msg.c_str(),msg.size(), 0) == -1 ) {
         err_print("Error while sending request");
@@ -149,87 +205,66 @@ string get_cont(string type, int sckt, string url, string file_path, string ver)
     }
     return resp_msg;
 }
-bool send_req(int sckt, string url, string file_path, int cnt, string ver)
+/*
+*/
+bool save_response(string response, bool chunked)
 {
-    string resp_msg = get_cont("HEAD", sckt, url, file_path, ver);
-    if (resp_msg == "ErrInMyGet")
-    {
+    size_t pos = response.find("\r\n\r\n");
+    response.erase(0, pos+4);
+    if (chunked) {
+        while (1)
+        {
+            break;
+        }
+    }
+    FILE *output;
+    if ( (output = fopen(file_name.c_str(), "w")) == NULL) {
+        err_print("Cannot create output file");
         return EXIT_FAILURE;
     }
-    // not working in funciot get_rex_matches problem with returning object
-    smatch head;
-    regex_search(resp_msg, head, head_rex);
-    //smatch head2=get_rex_matches(resp_msg, head_rex, true); head 2 magicaly working no idea why
-    for (unsigned int i = 0; i < head.size(); ++i)
+    if (fputs(response.c_str(), output) == EOF)
     {
-        cout<<"["<<i<<"]= "<<head[i]<<endl;
+        err_print("Error writing to output file");
+        return EXIT_FAILURE;
     }
-
-    // bad logic (long), find out different
-    
-    if (cnt == 1)
-    {
-        if (head[1]=="1") {
-            if (test_function()){
-                return EXIT_FAILURE;
-            }
-        }
-        else
-        {
-            if( send_req(sckt, url, file_path, ++cnt, "0") ){
-                return EXIT_FAILURE;
-            }
-        }
-    }
-    if ( cnt == 2 )
-    {
-        if ( head[1]=="0")
-        {
-            if (test_function()) {
-                return EXIT_FAILURE;
-            }
-        }
-        else 
-        {
-            err_print("Unexpected error");
-            return EXIT_FAILURE;
-        }
-    }
+    fclose(output);
     return EXIT_SUCCESS;
 }
-bool test_function ()
-{
-    for (int i = 1; i <= MAX_ATTEMPTS; ++i)
-    {
-        cout << i << endl;
-        if (i==4) {
-            return EXIT_SUCCESS;   
-        }
-    }
-    return EXIT_FAILURE;
-}
+/*
+*
+*/
 string get_default(string str, int type)
 {
     switch (type) 
     {
         case U_PORT : return (str.size() != 0) ? str : DEF_PORT ;
-        case U_PATH : return (str.size() != 0) ? str : DEF_PATH ;
         case F_NAME : return (str.size() != 0) ? str : DEF_NAME ;
+        case U_PATH : return (str.size() != 0) ? str : DEF_PATH ;
     }
     return NULL;
 }
+/*
+*
+*/
 void err_print(const char *msg)
 {
     cerr << msg << endl;    
 }
-smatch get_rex_matches(string input, regex rex, bool search)
+/*
+*
+*/
+string url_encode(string input)
+{
+    regex space(" ");
+    regex tilda("~");
+    string replacement[] = { "%20","%7E"};
+    input = regex_replace (input,space,replacement[0]);
+    input = regex_replace (input,tilda,replacement[1]);
+    return input;
+}
+smatch get_rex_matches(string input, regex rex)
 {
     smatch output;
-    if (search) {
-        regex_search(input, output, rex);
-    }
-    else {
-        regex_match(input, output, rex);
-    }
+    regex_match(input, output, rex);
     return output;
 }
